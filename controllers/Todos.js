1636dadbe2a7a4b39aafd8d8d708f8b09f17761d
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-12-17 16:39:43 +0530

    Update Todos.js

--- controllers/Todos.js
+++ controllers/Todos.js
@@ -1,27 +1,32 @@
 const { getAllTodo,createNewTodo,TodoById,deleteTodo } = require('../models/Todos')
 
 function getTodo(req,res){
-    const Todo = getAllTodo()
-    Todo.then((data)=>res.json({ data }))
+    const todos = getAllTodo()
+    console.log(todos)
+    todos.then((data)=>{
+         res.status(200).json({
+         message: "successfully fetched",
+         data: data })
+    })
 }
 
 function createTodo(req,res){
     const { task } = req.body
     const data = createNewTodo(task)
-    res.json({ msg: data})
+    res.status(201).json({ message: "successfully created"})
 }
 
 function updateTodoById(req,res){
     const id = req.params.id
     const { task } = req.body
     const data = TodoById({ id,task })
-    res.json({ msg: "success"})
+    res.status(201).json({ msg: "successfully updated"})
 }
 
 function deleteTodoById(req,res){
     const id = req.params.id
     const data = deleteTodo(id)
-    res.json({ msg: "success"})
+    res.status(201).json({ msg: "successfully deleted"})
 }
 
 module.exports = {
